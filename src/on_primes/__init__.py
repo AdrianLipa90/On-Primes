98cd8c07b3071a5be19da8ff707152c14155e466
Adrian Lipa <adrianlipa90@gmail.com>
2026-09-22 14:50:29 +0100

    code: export modular obstruction API

--- src/on_primes/__init__.py
+++ src/on_primes/__init__.py
@@ -13,15 +13,25 @@ from .dyadic import (
     sophie_step,
     v2,
 )
+from .modular import (
+    is_obstructed_by,
+    multiplicative_order_2,
+    obstruction_class,
+    obstruction_profile,
+)
 
 __all__ = [
     "cunningham_run",
     "dyadic_address",
     "fibre_value",
     "half_boundary_prime_preimages",
+    "is_obstructed_by",
     "is_prime",
     "log_fibre_coordinate",
     "log_prime_defect",
+    "multiplicative_order_2",
+    "obstruction_class",
+    "obstruction_profile",
     "odd_part",
     "prime_mask",
     "sophie_step",
