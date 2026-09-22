f9d198360eafb74bf47654349966112f48defdea
Adrian Lipa <adrianlipa90@gmail.com>
2026-09-22 14:48:41 +0100

    code: initialize on_primes package

---
+++ src/on_primes/__init__.py
@@ -0,0 +1,29 @@
+"""On Primes reference implementation."""
+
+from .dyadic import (
+    cunningham_run,
+    dyadic_address,
+    fibre_value,
+    half_boundary_prime_preimages,
+    is_prime,
+    log_fibre_coordinate,
+    log_prime_defect,
+    odd_part,
+    prime_mask,
+    sophie_step,
+    v2,
+)
+
+__all__ = [
+    "cunningham_run",
+    "dyadic_address",
+    "fibre_value",
+    "half_boundary_prime_preimages",
+    "is_prime",
+    "log_fibre_coordinate",
+    "log_prime_defect",
+    "odd_part",
+    "prime_mask",
+    "sophie_step",
+    "v2",
+]
